bc97e7cc278f07c40d52e9f8d2630a7cc96f9440
Alex Pham <alex.j.pham@gmail.com>
2023-05-15 12:28:38 -0500

    add main fiule

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+@app.get("/test")
+def read_root():
+    return {"Test endpoint"}
+
+@app.get("/submitbutton")
+def read_root():
+    return {"Thanks for clicking"}
+
